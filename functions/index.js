254e0904b488337f7a7e130cf5ee1af96d25196f
Wachiramet <wachiramet.pai@gmail.com>
2017-05-21 17:45:59 +0700

    send email

--- functions/index.js
+++ functions/index.js
@@ -11,8 +11,8 @@ var transporter = nodemailer.createTransport({
   secure: false,
   port: 25,
   auth: {
-    user: functions.config().gmail.email,
-    pass: functions.config().gmail.password
+    user: 'bangkok.issue@gmail.com',
+    pass: '123456bkk'
   }
 })
 
@@ -25,15 +25,33 @@ exports.countlikechange = functions.database.ref('/issues/{issueid}/votes').onWr
   admin.database().ref('/issues/' + event.params.issueid).once('value').then(function (snapshot) {
     console.log(snapshot.val())
     if (size === 1) {
+      let picture = ''
+      snapshot.val().photos.forEach(function(element) {
+        picture += '<img src="'+ element.img + '" width="200px" height="auto">'
+      })
       let mailOptions = {
         from: '"Fred Foo 👻" <foo@blurdybloop.com>', // sender address
-        to: '7sin@outlook.co.th', // list of receivers
-        subject: 'Hello ✔', // Subject line
+        to: 'wachiramet.pai@gmail.com', // list of receivers
+        subject: `แจ้งเพื่อพิจารณาแก้ไขปัญหา${snapshot.val().topic}`, // Subject line
         text: 'Hello world ?', // plain text body
-        html: '<b>Hello world ?</b>' + snapshot.val() // html body
+        html: ` <br> <br> <br> <br>
+<h4 align="center"><B>30 กุมภาพันธ์ 2660</B></h4>
+<br>
+<p><B>เรื่อง</B> แจ้งเพื่อพิจารณาแก้ไขปัญหา ${snapshot.val().topic}</p>
+<p><B>เรียน</B> กรมโยธาธิการและผังเมือง</p>
+<p><B>สิ่งที่แนบมาด้วย</B> รูปถ่ายสถานที่ที่เกิดปัญหา</p>
+<br>
+<p><dd>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ตามที่บริษัท Bangkok issue จำกัด ได้รับมอบหมายให้จัดทำแอปพลิเคชัน Bangkok issue ขึ้นเพื่อรับเรื่องร้องทุกข์เกี่ยวกับปัญหาที่พบเจอใน กทม.
+<p><dd>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ในการนี้แอปพลิเคชัน Bangkok issue ได้รับแจ้งปัญหาเรื่อง <u>${snapshot.val().topic} </u>สถานที่เกิดเหตุคือบริเวณ <u>${snapshot.val().location}</u> (<a href="https://www.google.com/maps/place/${snapshot.val().locationGps.lat}N+${snapshot.val().locationGps.lng}E/@${snapshot.val().locationGps.lat},${snapshot.val().locationGps.lng},14z">GPS</a>) โดยปัญหานี้ส่งผลให้ <u>${snapshot.val().description}</u> เพื่อความเป็นอยู่ที่ดีขึ้นของประชาชนจึงเรียนมาเพื่อพิจารณาแก้ไขปัญหาดังกล่าว</p>
+
+ <br>
+<p align="right">ขอแสดงความนับถือ</p>
+<p align="right">บริษัท Bangkok issue จำกัด</p>
+<br>
+${picture}` // html body
       }
       return transporter.sendMail(mailOptions).then(() => {
-        console.log('New unsubscription confirmation email sent to:', '"7sin@outlook.co.th"')
+        console.log('New unsubscription confirmation email sent to:', '"wachiramet.pai@gmail.com"')
       })
     }
   })
